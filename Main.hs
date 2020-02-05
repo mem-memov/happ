9b06ba828c46abbe065c9d18286da876ea21b869
Alexander Novikov <mem-memov@yandex.ru>
2020-02-05 07:40:49 +0300

    App monad

--- Main.hs
+++ Main.hs
@@ -1,10 +1,15 @@
 
-data Environment = Environment
-data Log = Log
-data Error = Fine | Error
-data Input = Input
-data Output = Output
-data App = App Environment Input Output Error Log
+data App a = App a
+
+instance Functor App where
+  fmap f (App x) = App (f x)
+
+instance Applicative App where
+  pure x = App x
+  (App f) (<*>) (App x) = App (f x) 
+
+instance Monad App where
+  (App x) (>>=) f = f x
 
 main :: IO ()
 main = do
