4d678331fe4616d83683c3b4dfccf1a5d52bacb9
Alexander Novikov <mem-memov@yandex.ru>
2020-02-05 09:15:04 +0300

    app structure

--- Main.hs
+++ Main.hs
@@ -1,15 +1,55 @@
+data AppState = AppState
+data AppConfiguration = AppConfiguration
+data AppError = AppNoError | AppError
 
-data App a = App a
+data App a = App {
+  getAppState :: AppState
+  , getAppConfiguration :: AppConfiguration
+  , getAppError :: AppError
+}
+
+app :: App ()
+app = App ()
 
 instance Functor App where
-  fmap f (App x) = App (f x)
+  fmap f (App x) = 
+    let 
+      appConfiguration = getAppConfiguration x
+      appError = getAppError x
+      y = case appError of
+        AppNoError -> App (f x)
+        AppError -> App x
+      newAppConfiguration = getAppConfiguration y
+      result = if newAppConfiguration == appConfiguration then y else x { getAppError = AppError }
+    in
+      result
 
 instance Applicative App where
   pure x = App x
-  (App f) (<*>) (App x) = App (f x) 
+  (App f) (<*>) (App x) = 
+    let 
+      appConfiguration = getAppConfiguration x
+      appError = getAppError x
+      y = case appError of
+        AppNoError -> App (f x)
+        AppError -> App x
+      newAppConfiguration = getAppConfiguration y
+      result = if newAppConfiguration == appConfiguration then y else x { getAppError = AppError }
+    in
+      result
 
 instance Monad App where
-  (App x) (>>=) f = f x
+  (App x) (>>=) f =
+    let 
+      appConfiguration = getAppConfiguration x
+      appError = getAppError x
+      y = case appError of
+        AppNoError -> f x
+        AppError -> App x
+      newAppConfiguration = getAppConfiguration y
+      result = if newAppConfiguration == appConfiguration then y else x { getAppError = AppError }
+    in
+      result
 
 main :: IO ()
 main = do
